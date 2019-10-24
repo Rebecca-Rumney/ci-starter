867d573c2b9532c0cde71e3f9e107c48ed91f3d5
sabs-r3 <examle@example.com>
2019-10-24 15:15:43 +0100

    Made it fail

--- test/test_clock.py
+++ test/test_clock.py
@@ -8,7 +8,7 @@ class MyTestCase(unittest.TestCase):
 
     @patch('time.time', return_value=1571871846.8861961)
     def test_the_time(self, mock):
-        self.assertEqual(clock.whats_the_time(), 1571871845.8861962)
+        self.assertEqual(clock.whats_the_time(), 1571871835.8861962)
 
 
 if __name__ == '__main__':
